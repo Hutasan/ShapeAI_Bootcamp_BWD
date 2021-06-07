ea3674c815ad634d79ccb1b77ceca445d1887ebb
Hutasan <69388372+Hutasan@users.noreply.github.com>
2021-06-07 10:41:35 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "rect";
+
+function Header() {
+  return (
+    <header>
+      <h1>ShapeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "rect";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>javascript and React.js</h1>
+      <p>This was an amazing bootcamp take up by Shaurya Sinha.</p>
+    </div>
+  );
+}
+
+export default Note;
